405e8bcd8d8f97a2d12b0845c411b04407e47dd5
Edson Junior <edson.junior@mereo.com>
2024-02-24 14:21:23 -0300

    Cria os relacionamentos nas tabelas com a migrate

--- database/migrations/2024_02_24_165153_create_carros_table.php
+++ database/migrations/2024_02_24_165153_create_carros_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('carros', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->string('place', 10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
             $table->timestamps();
+
+            //Relacionamento 1 para muitos
+            //1 modelo tem vários carros
+            $table->foreign('modelo_id')->references('id')->on('modelos');
         });
     }
 
