b40891bdc34ebad53656a6b1b67a5a95bcd81e06
Edson Junior <edson.junior@mereo.com>
2024-02-24 16:31:25 -0300

    Removendo métodos create e edit (Controllers)
    Esse métodos estão sendo removido pois estou utilizando o apiResource
    Ao invez da resource nas rotas.
    
    (Laravel)
    Estou utilizando nesse projeto o SQLite para salvar informações de banco.

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -15,17 +15,7 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return 'index.php';
     }
 
     /**
@@ -46,17 +36,6 @@ class ClienteController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Cliente $cliente)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cliente $cliente)
     {
         //
     }
